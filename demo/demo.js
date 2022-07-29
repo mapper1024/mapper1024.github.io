e130bced41b449788a902f1fa376391786c0b99c
Benjamin Leskey <linewriter1024@gmail.com>
2022-07-29 16:57:28 -0400

    Add ability to make a blank map in the demo.

--- demo/demo.js
+++ demo/demo.js
@@ -1,7 +1,22 @@
 import { Mapper, SqlJsMapBackend } from "./mapper/mapper.js";
 
-const map = new SqlJsMapBackend("./mapper/samples/sample_map.map");
-map.load().then(function() {
-	const mapper = new Mapper(map);
-	mapper.render(document.getElementById("mapper"));
-});
+let renderedMap;
+
+function loadMap(map) {
+	if(renderedMap) {
+		renderedMap.disconnect();
+	}
+	map.load().then(function() {
+		const mapper = new Mapper(map);
+		renderedMap = mapper.render(document.getElementById("mapper"));
+		renderedMap.hooks.add("draw_help", function(options) {
+			options.infoLine("Shift+N to make a blank map.");
+		});
+
+		renderedMap.registerKeyboardShortcut((context, event) => event.key === "N", async () => {
+			loadMap(new SqlJsMapBackend());
+		});
+	});
+}
+
+loadMap(new SqlJsMapBackend("./mapper/samples/sample_map.map"));
