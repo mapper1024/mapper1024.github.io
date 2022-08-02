8983917d225db99511a4c06e6c52f9f054693a8d
Benjamin Leskey <linewriter1024@gmail.com>
2022-08-02 17:19:23 -0400

    Add screenshot management script and new 0.1.8 screenshots

---
+++ screenshots/update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -ex
+
+DEFAULT=sample_map_0.1.8
+
+if ! [ -f update.sh ] || ! [ -f ../index.html ]; then
+	echo "Must run $0 in the screenshots directory."
+	exit 1
+fi
+
+find -maxdepth 1 -name "*.thumb.png" -delete
+rm -f default.png default.thumb.png
+
+find -name "*.png" -prune | while read n; do
+	convert -resize 25% "$n" "${n%.*}.thumb.png"
+done
+
+ln -s "$DEFAULT.png" default.png
+ln -s "$DEFAULT.thumb.png" default.thumb.png
