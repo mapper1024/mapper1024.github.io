695a9645333082c4089a90a7f351697d88a5557d
Benjamin Leskey <linewriter1024@gmail.com>
2023-01-03 12:58:53 -0500

    new screenshot

--- screenshots/update.sh
+++ screenshots/update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-DEFAULT=sample_map_0.3.2
+DEFAULT=sample_map_0.4.0
 
 if ! [ -f update.sh ] || ! [ -f ../index.html ]; then
 	echo "Must run $0 in the screenshots directory."
