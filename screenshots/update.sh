62313477c9988379de3759a72ea58bb049360157
Benjamin Leskey <ben@benleskey.com>
2023-01-21 15:46:28 -0500

    0.5.0 screenshot

--- screenshots/update.sh
+++ screenshots/update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-DEFAULT=sample_map_0.4.0
+DEFAULT=sample_map_0.5.0
 
 if ! [ -f update.sh ] || ! [ -f ../index.html ]; then
 	echo "Must run $0 in the screenshots directory."
@@ -12,8 +12,10 @@ find -maxdepth 1 -name "*.thumb.png" -delete
 rm -f default.png default.thumb.png
 
 find -name "*.png" -prune | while read n; do
-	convert -resize 25% "$n" "${n%.*}.thumb.png"
+	convert -resize 25% "$n" "${n%.*}.thumb.png" &
 done
+
+wait
 
 ln -s "$DEFAULT.png" default.png
 ln -s "$DEFAULT.thumb.png" default.thumb.png
